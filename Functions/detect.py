46b80227652eae0a63c5518c8133948a8fb5bd09
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-14 20:12:15 +0100

    add(detect) : Création de fonction et ajout d'un paramètre pour l'id de la caméra

--- Functions/detect.py
+++ Functions/detect.py
@@ -6,16 +6,19 @@ import numpy as np
 
 log = logging.getLogger("main")
 
-def detect(type, show=False):
+
+def detect(webcam, classes, show=False):
     """
     Fonction de détection
-    :param type: type de détection (0: personnes, 1: vélos)
+    :param webcam: webcam à utiliser
+    :param classes: type de détection (0: personnes, 1: vélos)
+    :param show: affichage de la détection (True/False) (optionnel)
     :return: None
     Enregistre les résultats dans un fichier csv avec comme entête:
     date,occurence,type,positions
     """
     log.debug("Début de la détection")
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(webcam)
     if show:
         log.warning("Affichage des résultats activé, pour fermer la fenêtre, appuyez sur la touche 'q'")
 
@@ -25,39 +28,42 @@ def detect(type, show=False):
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
         # paramètres du model
-        model.classes = type
-        model.conf = 0.25  # NMS confidence threshold
+        model.classes = classes
+        model.conf = 0.50  # NMS confidence threshold
         model.iou = 0.45  # NMS IoU threshold
         model.agnostic = False  # NMS class-agnostic
         model.multi_label = False  # NMS multiple labels per box
         model.max_det = 1000  # maximum number of detections per image
-        model.amp = False  # Automatic Mixed Precision (AMP) inference  
+        model.amp = False  # Automatic Mixed Precision (AMP) inference
 
         # detection
-        results = model(frame) # 'OUTPUT/photo.jpg'
+        results = model(frame)  # 'OUTPUT/photo.jpg'
 
-        # affichage des résultats
+        generate_csv(results, classes)
+
+        # affichage des images
         if show:
             cv2.imshow('YOLO', np.squeeze(results.render()))
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
-        log.debug("Traitement des résultats")
-        # enregistrement et traitement des résultats
-        data = results.pandas().xyxy[0]
-        data = data.drop(columns=['name', 'confidence', 'class'])
-        date = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
-        tab_of_positions = data.values.tolist()
-        nb_personnes = len(tab_of_positions)
-
-        # enregistrement des données dans un fichier csv
-        with open('OUTPUT/data.csv', 'a') as f:
-            # si le fichier est vide, on écrit l'entête
-            if f.tell() == 0:
-                f.write("date,occurence,type,positions\n")
-            f.write(date + ',' + str(nb_personnes) + ',' + str(type) + ',' + str(tab_of_positions) + '\r')
-        log.debug("Résultats traités")
-
     cam.release()
     cv2.destroyAllWindows()
     log.debug("Detection terminée")
+
+
+def generate_csv(results, classes):
+    log.debug("Traitement des résultats")
+    # enregistrement et traitement des résultats
+    data = results.pandas().xyxy[0]
+    data = data.drop(columns=['name', 'confidence', 'class'])
+    date = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
+    tab_of_positions = data.values.tolist()
+    nb_personnes = len(tab_of_positions)
+    # enregistrement des données dans un fichier csv
+    with open('OUTPUT/data.csv', 'a') as f:
+        # si le fichier est vide, on écrit l'entête
+        if f.tell() == 0:
+            f.write("date,occurence,type,positions\n")
+        f.write(date + ',' + str(nb_personnes) + ',' + str(classes) + ',' + str(tab_of_positions) + '\r')
+    log.debug("Résultats traités")
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
 from CustomFormatter import CustomFormatter
 from Functions import detect
-import cv2
 
 log = logging.getLogger("main")
 log.setLevel('DEBUG')
@@ -10,6 +9,5 @@ ch.setLevel('DEBUG')
 ch.setFormatter(CustomFormatter())
 log.addHandler(ch)
 
-
 if __name__ == "__main__":
-    detect.detect(0, True)
+    detect.detect(0, 0, True)
