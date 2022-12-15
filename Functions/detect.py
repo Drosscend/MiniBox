66ab3ffbec3afe170f5b9fcce8c64cccb8e69bfb
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-15 21:52:51 +0100

    feat(detect) : Amélioration du système de détection en utilisant SORT (Simple Online and Realtime Tracking). Enregistrement des informations dans le fichier CV uniquement quand c'est une nouvelle personne.

--- Functions/detect.py
+++ Functions/detect.py
@@ -1,10 +1,17 @@
+import logging
 import time
 import torch
-import logging
 import cv2
-import numpy as np
+from Functions import utils
+from Functions.sort import Sort
 
 log = logging.getLogger("main")
+
+# Dictionnaire pour stocker les identifiants des personnes suivies
+tracked_people = {}
+
+# Initialisation de la librairie Sort pour suivre les personnes détectées
+model_sort = Sort()
 
 
 def detect(webcam, classes, show=False, debug=False):
@@ -13,6 +20,7 @@ def detect(webcam, classes, show=False, debug=False):
     :param webcam: webcam à utiliser
     :param classes: type de détection (0: personnes, 1: vélos)
     :param show: affichage de la détection (True/False) (optionnel)
+    :param debug: affichage des logs de débug (True/False) (optionnel)
     :return: None
     Enregistre les résultats dans un fichier csv avec comme entête:
     date,occurence,type,positions
@@ -29,21 +37,39 @@ def detect(webcam, classes, show=False, debug=False):
 
         # paramètres du model
         model.classes = classes
-        model.conf = 0.50  # NMS confidence threshold
+        model.conf = 0.25  # NMS confidence threshold
         model.iou = 0.45  # NMS IoU threshold
         model.agnostic = False  # NMS class-agnostic
-        model.multi_label = False  # NMS multiple labels per box
-        model.max_det = 1000  # maximum number of detections per image
-        model.amp = False  # Automatic Mixed Precision (AMP) inference
+        model.multi_label = True  # NMS multiple labels per box
+        model.max_det = 20  # maximum number of detections per image
+        model.amp = True  # Automatic Mixed Precision (AMP) inference
 
         # detection
-        results = model(frame)  # 'OUTPUT/photo.jpg'
+        results = model(frame)
 
-        generate_csv(results, classes)
+        # Utilisation de la librairie Sort pour suivre les personnes détectées
+        detections = results.pred[0].numpy()
+        track = model_sort.update(detections)
+
+        # Détection des nouvelles personnes
+        new_people_count = 0
+        for j in range(len(track.tolist())):
+            coords = track.tolist()[j]
+            name_idx = int(coords[4])
+            # Si l'identifiant de la personne n'est pas déjà enregistré, c'est une nouvelle personne
+            if name_idx not in tracked_people:
+                new_people_count += 1
+                tracked_people[name_idx] = 1
+
+        generate_csv(new_people_count, classes)
 
         # affichage des images
         if show:
-            cv2.imshow('YOLO', np.squeeze(results.render()))
+            confidence = 0
+            # définie à a le pourcentage de sureté de la détection
+            if len(results.pred[0]) > 0:
+                confidence = round(results.pred[0][0][4].item(), 2)
+            show_output(frame, track, confidence)
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
@@ -52,21 +78,30 @@ def detect(webcam, classes, show=False, debug=False):
     log.debug("Detection terminée")
 
 
-def generate_csv(results, classes):
+def generate_csv(occurence, classes):
     log.debug("------------------------------------------")
     log.debug("Traitement des résultats")
-    # enregistrement et traitement des résultats
-    data = results.pandas().xyxy[0]
-    data = data.drop(columns=['name', 'confidence', 'class'])
-    date = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
-    tab_of_positions = data.values.tolist()
-    nb_personnes = len(tab_of_positions)
-    log.debug("Nombre de personnes détectées: " + str(nb_personnes))
-    # enregistrement des données dans un fichier csv
-    with open('OUTPUT/data.csv', 'a') as f:
-        # si le fichier est vide, on écrit l'entête
-        if f.tell() == 0:
-            f.write("date,occurence,type,positions\n")
-        f.write(date + ',' + str(nb_personnes) + ',' + str(classes) + ',' + str(tab_of_positions) + '\r')
+    # Si au moins une personne a été détectée, on enregistre les résultats dans le fichier CSV
+    if occurence > 0:
+        date = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
+        # enregistrement des données dans un fichier csv
+        with open('OUTPUT/data.csv', 'a') as f:
+            # si le fichier est vide, on écrit l'entête
+            if f.tell() == 0:
+                f.write("date,occurence,type\n")
+            f.write(date + ',' + str(occurence) + ',' + str(classes) + '\r')
     log.debug("Résultats traités")
     log.debug("------------------------------------------")
+
+
+def show_output(image, track, confidence):
+    # Dessin des rectangles autour des personnes suivies et affichage de leur identifiant
+    for j in range(len(track.tolist())):
+        coords = track.tolist()[j]
+        x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+        name_idx = int(coords[4])
+        name = "ID: {}".format(str(name_idx)) + " - " + str(confidence)
+        color = utils.random_color(name_idx)
+        cv2.rectangle(image, (x1, x1), (x2, y2), color, 2)
+        cv2.putText(image, name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    cv2.imshow('Image', image)
