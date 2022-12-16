417f796e35833f32a270ef28bd82e1bacd5b09e5
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-16 11:33:38 +0100

    fix(detect) : amélioration de la détection et de l'affichage

--- Functions/detect.py
+++ Functions/detect.py
@@ -3,13 +3,14 @@ import time
 import torch
 import cv2
 import os
+import numpy as np
 from Functions import utils
 from Functions.sort import Sort
 
 log = logging.getLogger("main")
 
 # Dictionnaire pour stocker les identifiants des personnes suivies
-tracked_people = {}
+tracked_object = {}
 
 # Initialisation de la librairie Sort pour suivre les personnes détectées
 model_sort = Sort()
@@ -19,7 +20,7 @@ def detect(webcam, classes, show=False, debug=False):
     """
     Fonction de détection
     :param webcam: webcam à utiliser
-    :param classes: type de détection (0: personnes, 1: vélos)
+    :param classes: type de détection (0: personnes, 1: vélos) ou liste de types
     :param show: affichage de la détection (True/False) (optionnel)
     :param debug: affichage des logs de débug (True/False) (optionnel)
     :return: None
@@ -32,7 +33,7 @@ def detect(webcam, classes, show=False, debug=False):
         log.warning("Affichage des résultats activé, pour fermer la fenêtre, appuyez sur la touche 'q'")
 
     while cam.isOpened():
-        ret, frame = cam.read()
+        _, frame = cam.read()
         # création du model
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s', verbose=debug)
 
@@ -49,28 +50,29 @@ def detect(webcam, classes, show=False, debug=False):
         results = model(frame)
 
         # Utilisation de la librairie Sort pour suivre les personnes détectées
-        detections = results.pred[0].numpy()
+        detections = np.array(results.xyxy[0][:, :4])
+
+        # Si aucune personne n'est détectée, on passe à l'image suivante
+        if len(detections) == 0:
+            continue
+
         track = model_sort.update(detections)
 
         # Détection des nouvelles personnes
-        new_people_count = 0
+        new_object_count = 0
         for j in range(len(track.tolist())):
             coords = track.tolist()[j]
             name_idx = int(coords[4])
             # Si l'identifiant de la personne n'est pas déjà enregistré, c'est une nouvelle personne
-            if name_idx not in tracked_people:
-                new_people_count += 1
-                tracked_people[name_idx] = 1
+            if name_idx not in tracked_object:
+                new_object_count += 1
+                tracked_object[name_idx] = 1
 
-        generate_csv(new_people_count, classes)
+        generate_csv(new_object_count, classes)
 
         # affichage des images
         if show:
-            confidence = 0
-            # définie à a le pourcentage de sureté de la détection
-            if len(results.pred[0]) > 0:
-                confidence = round(results.pred[0][0][4].item(), 2)
-            show_output(frame, track, confidence)
+            show_output(frame, track)
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
@@ -98,14 +100,16 @@ def generate_csv(occurence, classes):
     log.debug("------------------------------------------")
 
 
-def show_output(image, track, confidence):
-    # Dessin des rectangles autour des personnes suivies et affichage de leur identifiant
-    for j in range(len(track.tolist())):
-        coords = track.tolist()[j]
-        x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+def show_output(image, track):
+    for i in range(len(track.tolist())):
+        coords = track.tolist()[i]
         name_idx = int(coords[4])
-        name = "ID: {}".format(str(name_idx)) + " - " + str(confidence)
+        x1 = int(coords[0])
+        y1 = int(coords[1])
+        x2 = int(coords[2])
+        y2 = int(coords[3])
         color = utils.random_color(name_idx)
-        cv2.rectangle(image, (x1, x1), (x2, y2), color, 2)
-        cv2.putText(image, name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    cv2.imshow('Image', image)
+        cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        cv2.putText(image, str(name_idx), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+    
+    cv2.imshow('YOLO', image)
