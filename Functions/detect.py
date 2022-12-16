dc2e50490815adb2f78899cbfe5fdfcf15ac5f43
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-12-16 21:00:33 +0100

    fix(detect) : optimisation et correction de bug

--- Functions/detect.py
+++ Functions/detect.py
@@ -5,7 +5,7 @@ import cv2
 import os
 import numpy as np
 from Functions import utils
-from Functions.sort import Sort
+from Functions import sort
 
 log = logging.getLogger("main")
 
@@ -13,40 +13,34 @@ log = logging.getLogger("main")
 tracked_object = {}
 
 # Initialisation de la librairie Sort pour suivre les personnes détectées
-model_sort = Sort()
+model_sort = sort.Sort()
 
 
-def detect(webcam, classes, show=False, debug=False):
+def detect(video_capture, classes, interval=10, show=False, debug=False):
     """
     Fonction de détection
-    :param webcam: webcam à utiliser
+    Enregistre les résultats dans un fichier csv avec comme entête:
+    date,occurence,type,positions
+
+    :param video_capture: objet cv2.VideoCapture pour la caméra
     :param classes: type de détection (0: personnes, 1: vélos) ou liste de types
+    :param interval: intervalle de temps entre chaque détection
     :param show: affichage de la détection (True/False) (optionnel)
     :param debug: affichage des logs de débug (True/False) (optionnel)
     :return: None
-    Enregistre les résultats dans un fichier csv avec comme entête:
-    date,occurence,type,positions
     """
     log.debug("Début de la détection")
-    cam = cv2.VideoCapture(webcam)
-    if show:
-        log.warning("Affichage des résultats activé, pour fermer la fenêtre, appuyez sur la touche 'q'")
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', verbose=debug)
+    model.classes = classes
+    model.conf = 0.25
+    model.iou = 0.45
+    model.agnostic = False
+    model.multi_label = True
+    model.max_det = 20
+    model.amp = True
+    while video_capture.isOpened():
+        _, frame = video_capture.read()
 
-    while cam.isOpened():
-        _, frame = cam.read()
-        # création du model
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', verbose=debug)
-
-        # paramètres du model
-        model.classes = classes
-        model.conf = 0.25  # NMS confidence threshold
-        model.iou = 0.45  # NMS IoU threshold
-        model.agnostic = False  # NMS class-agnostic
-        model.multi_label = True  # NMS multiple labels per box
-        model.max_det = 20  # maximum number of detections per image
-        model.amp = True  # Automatic Mixed Precision (AMP) inference
-
-        # detection
         results = model(frame)
 
         # Utilisation de la librairie Sort pour suivre les personnes détectées
@@ -76,16 +70,27 @@ def detect(webcam, classes, show=False, debug=False):
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
-    cam.release()
+        # Pause entre chaque détection
+        log.debug("Pause de " + str(interval) + " secondes")
+        time.sleep(interval)
+
+    video_capture.release()
     cv2.destroyAllWindows()
     log.debug("Detection terminée")
 
 
 def generate_csv(occurence, classes):
-    log.debug("------------------------------------------")
-    log.debug("Traitement des résultats")
+    """
+    Enregistre les résultats de la détection dans un fichier CSV.
+
+    :param occurence: nombre de personnes ou vélos détectés
+    :param classes: type de détection (0: personnes, 1: vélos)
+    :return: None
+    """
     # Si au moins une personne a été détectée, on enregistre les résultats dans le fichier CSV
     if occurence > 0:
+        log.debug("Nouvelle objet détecté " + str(occurence) + " fois" + " de type " + str(classes))
+
         date = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
         # verifie que le dosser OUTPUT existe et le crée si ce n'est pas le cas
         if not os.path.exists("OUTPUT"):
@@ -96,20 +101,45 @@ def generate_csv(occurence, classes):
             if f.tell() == 0:
                 f.write("date,occurence,type\n")
             f.write(date + ',' + str(occurence) + ',' + str(classes) + '\r')
-    log.debug("Résultats traités")
-    log.debug("------------------------------------------")
 
 
 def show_output(image, track):
+    """
+    Affiche une image avec des rectangles entourant les personnes détectées et en affichant leur identifiant près de chaque personne.
+
+    :param image: image à afficher
+    :param track: informations sur les personnes détectées, y compris leurs coordonnées et leur identifiant
+    :return: None
+    """
+
+    # Pour chaque personne détectée, dessine un rectangle autour de la personne et affiche son identifiant
     for i in range(len(track.tolist())):
+        # Récupère les informations sur la personne
         coords = track.tolist()[i]
-        name_idx = int(coords[4])
+        name_idx = int(coords[4])  # Identifiant de la personne
         x1 = int(coords[0])
         y1 = int(coords[1])
         x2 = int(coords[2])
         y2 = int(coords[3])
+        # Génère une couleur aléatoire pour chaque personne en utilisant son identifiant
         color = utils.random_color(name_idx)
+        # Dessine un rectangle autour de la personne
         cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        # Affiche l'identifiant de la personne près de la personne
         cv2.putText(image, str(name_idx), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-    
+
+    # Affiche l'image modifiée à l'écran
     cv2.imshow('YOLO', image)
+
+
+def main(webcam, classes, interval, show, debug):
+    # Initialisation de la caméra
+    video_capture = cv2.VideoCapture(webcam)
+
+    # Vérification de l'ouverture de la caméra
+    if not video_capture.isOpened():
+        log.error("Impossible d'ouvrir la webcam")
+        exit()
+
+    # Détection des personnes
+    detect(video_capture, classes, interval, show, debug)
--- main.py
+++ main.py
@@ -15,6 +15,7 @@ parser = argparse.ArgumentParser(prog='Mini Box', description='Projet de mémoir
 parser.add_argument('-w', '--webcam', type=int, default=0, help='Webcam à utiliser', required=False, choices=[0, 1])
 parser.add_argument('-c', '--classes', type=int, default=0, help='Type de détection (0: personnes, 1: vélos)',
                     required=False, choices=[0, 1])
+parser.add_argument('-i', '--interval', type=int, default=1, help='Intervalle entre chaque capture en secondes', required=False)
 parser.add_argument('-s', '--show', help='Affichage de la détection', required=False, action='store_true')
 parser.add_argument('-d', '--debug', help='Mode debug', required=False, action='store_true')
 args = parser.parse_args()
@@ -27,6 +28,7 @@ if __name__ == "__main__":
     log.info("Paramètres :")
     log.info("Webcam : " + str(args.webcam))
     log.info("Classes : " + str(args.classes))
+    log.info("Intervalle : " + str(args.interval) + " secondes")
     log.info("Affichage : " + str(args.show))
     log.info("Debug : " + str(args.debug))
 
@@ -37,4 +39,4 @@ if __name__ == "__main__":
         log.debug("Mode debug activé")
 
     # lancement de la détection
-    detect.detect(args.webcam, args.classes, args.show, args.debug)
+    detect.main(args.webcam, args.classes, args.interval, args.show, args.debug)
