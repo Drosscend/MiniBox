7b33f565f594c644ee45187183b1d2b9ca1940f3
Noemie Tandol <noemie.tandol@gmail.com>
2022-12-18 14:36:50 +0100

    feat(WebGraph): ajout validation seulement si le fichier est un .csv

--- www/WebGraph/WebGraphique/views.py
+++ www/WebGraph/WebGraphique/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from WebGraphique.forms.UploadFileForm import UploadFileForm
-from WebGraphique.models import CSVFile
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
 
-        fileName = form.save()
-        return render(request, 'WebGraphique/index.html', {'form': form, 'file': str(fileName.file)})
+        if str(request.FILES['file'])[-4:] == ".csv":
+            fileName = form.save()
+            return render(request, 'index.html', {'form': form, 'file': str(fileName.file)})
+        else :
+            return render(request, 'index.html', {'form': form, 'error': "Le fichier doit être un .csv"})
     else:
         form = UploadFileForm()
-    return render(request, 'WebGraphique/index.html', {'form': form})
+    return render(request, 'index.html', {'form': form})
