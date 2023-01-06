cecbb0ca05c32116752680ed564f9d10d5aebf89
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-01-06 16:55:02 +0100

    fix(detect) : Fix d'un problème important lors de la détection de plusieurs objects en même temps

--- Functions/detect.py
+++ Functions/detect.py
@@ -41,6 +41,8 @@ def generate_csv(current):
     # Parcours de la liste des identifiants d'objets
     for obj_id in current:
         obj = tracked_objects.get(obj_id)
+        if obj.direction is None:
+            continue
         if obj.direction == "top-left":
             top_left += 1
         elif obj.direction == "top-right":
@@ -155,22 +157,22 @@ def detect(video_capture, classes, interval, show, debug, only_new):
 
             current.append(obj_id)
 
-        if current:
-            # Si l'objet n'a pas encore été suivi, c'est un nouvel objet
-            found = False
-            for tracked_object in tracked_objects.tracked_objects:
-                if tracked_object.obj_id == obj_id:
-                    found = True
-                    tracked_object.update_position(conf, x1, y1, x2, y2)
-                    break
-            if not found:
-                color = utils.random_color(obj_id)
-                tracked_objects.add(obj_id, conf, x1, y1, x2, y2, color)
-                new_detected = True
-                log.debug("Nouvel objet détecté: " + str(obj_id))
-            log.debug("Objets détectés: " + str(current))
-        else:
-            log.debug("Aucun objet détecté")
+            if current:
+                # Si l'objet n'a pas encore été suivi, c'est un nouvel objet
+                found = False
+                for tracked_object in tracked_objects.tracked_objects:
+                    if tracked_object.obj_id == obj_id:
+                        found = True
+                        tracked_object.update_position(conf, x1, y1, x2, y2)
+                        break
+                if not found:
+                    color = utils.random_color(obj_id)
+                    tracked_objects.add(obj_id, conf, x1, y1, x2, y2, color)
+                    new_detected = True
+                    log.debug("Nouvel objet détecté: " + str(obj_id))
+                log.debug("Objets détectés: " + str(current))
+            else:
+                log.debug("Aucun objet détecté")
 
         # Enregistrement des résultats dans un fichier csv si une nouvelle personne est détectée
         if only_new:
