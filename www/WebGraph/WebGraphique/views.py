c59b083cc50a843ffdfc3f98eae91c3ebd85d77e
Noemie Tandol <noemie.tandol@gmail.com>
2023-01-07 15:07:24 +0100

    feat(WebGraph): ajout de la suppression des anciens CSV + lecture du fichier

--- www/WebGraph/WebGraphique/views.py
+++ www/WebGraph/WebGraphique/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from WebGraphique.forms.UploadFileForm import UploadFileForm
+from WebGraphique.models import CSVFile
+from WebGraph.settings import BASE_DIR
+import csv
+import os
 
 # Create your views here.
 def index(request):
@@ -7,8 +11,23 @@ def index(request):
         form = UploadFileForm(request.POST, request.FILES)
 
         if str(request.FILES['file'])[-4:] == ".csv":
+
+
+            # Suppression des anciens CSV
+            allCSV = CSVFile.objects.all()
+            for aCSV in allCSV:
+                os.remove(str(BASE_DIR) + "\\" + str(aCSV.file))
+                aCSV.delete()
+
+            # Enregistrement du nouveau
             fileName = form.save()
-            return render(request, 'index.html', {'form': form, 'file': str(fileName.file)})
+
+            # Récupération des lines du fichier
+            f = open( str(BASE_DIR) + "\\" + str(fileName.file))
+            lines = list(csv.reader(f))
+            f.close()
+
+            return render(request, 'index.html', {'form': form, 'file': str(fileName.file), 'lines' : lines})
         else :
             return render(request, 'index.html', {'form': form, 'error': "Le fichier doit être un .csv"})
     else:
